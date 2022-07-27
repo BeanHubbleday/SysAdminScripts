39647e08fd77ef342b7d11a96f559c022d11d260
Jack Hubbleday <38787899+BeanHubbleday@users.noreply.github.com>
2022-07-27 12:54:40 +0100

    Uploaded create a vpn account script

---
+++ Create A VPN Account.ps1
@@ -0,0 +1,10 @@
+﻿Install-Module -Name VPNCredentialsHelper
+
+$name = Read-Host -Prompt 'Enter Customer Name'
+$address = Read-Host -Prompt 'Enter The Customers IP Address'
+$username =  Read-Host -Prompt 'Enter The Users Username Here'
+$plainpassword =  Read-Host -Prompt 'Enter The Users Password Here'
+
+Add-VpnConnection -Name $name -ServerAddress $address -TunnelType "Pptp" -EncryptionLevel "Required" -AuthenticationMethod MSChapv2 -SplitTunneling -RememberCredential -PassThru
+
+Set-VpnConnectionUsernamePassword -connectionname $name -username $username -password $plainpassword
