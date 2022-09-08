693de2969a61918289427b06286363d0c49827c2
Jack Hubbleday <jack.hubbleday@bespokecomputing.com>
2022-09-08 10:00:06 +0100

    SPAG

--- View Status Of Online Archive In 365.ps1
+++ View Status Of Online Archive In 365.ps1
@@ -11,7 +11,7 @@ $archivingUser = Read-Host -Prompt "Enter The Email Address Of The User You Want
 #Show archive size & refresh every X seconds
 $aMadeUpVariable = 0
 $seconds = Read-Host -Prompt "How Often Do You Want To Refresh The Status (Seconds)?"
-Write-Host "WHen You're Finished Viewing The Status, Simply Hit Ctrl+C Or Close PowerShell"
+Write-Host "When You're Finished Viewing The Status, Simply Hit Ctrl+C Or Close PowerShell"
 Do {
     Get-MailboxStatistics -Identity $archivingUser -Archive | Select DisplayName, TotalItemSize, ItemCount
     Start-Sleep -Seconds $seconds
