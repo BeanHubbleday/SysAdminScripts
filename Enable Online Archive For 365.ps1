fb8af8ca478a21fa99984dc307c824e840cc0c65
Jack Hubbleday <jack.hubbleday@bespokecomputing.com>
2022-09-08 09:55:46 +0100

    SPAG

--- Enable Online Archive For 365.ps1
+++ Enable Online Archive For 365.ps1
@@ -15,7 +15,7 @@ Write-Host "Archiving Enabled..."
 #Show archive size & refresh every X seconds
 $aMadeUpVariable = 0
 $seconds = Read-Host -Prompt "How Often Do You Want To Refresh The Status (Seconds)?"
-Write-Host "When You're Finished Viewing The Status, Simply Hit Ctrl+X Or Close PowerShell"
+Write-Host "When You're Finished Viewing The Status, Simply Hit Ctrl+C Or Close PowerShell"
 Do {
     Get-MailboxStatistics -Identity $archivingUser -Archive | Select DisplayName, TotalItemSize, ItemCount
     Start-Sleep -Seconds $seconds
